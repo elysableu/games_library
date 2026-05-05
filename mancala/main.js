19e014a6cd10c87d2400e756cd2350f831b7a53d
elysableu <elysabward@gmail.com>
2026-05-05 15:20:12 -0600

    Add base files for mancala

--- mancala/main.js
+++ mancala/main.js
@@ -22,12 +22,55 @@
             - CHAIN TURNS: always look for moves that end in your store to keep up your momentum
 */
 
-class Game {
-    constructor() {
+import { input, select, confirm, number } from '@inquirer/prompts';
 
+class Game {
+    static totalStones = 48;
+    static boardDimensions = {w: 8, h: 2};
+
+    constructor(computerOpponent) {
+        this.board = new Board(boardDimensions);
+        this.players = [];
+        this.computerOpponent = computerOpponent;
+        this.humanPlayer = null;
+        this.resolved = false;
+
+        this.build();
     }
 
     static async init() {
+        const computerOpponent = false;
+
+        console.log('Let\'s play mancala!');
+        if (await confirm({message: 'Ready?'})) {
+            computerOpponent = await select({
+                message: 'Who are you playing against?',
+                choices: [
+                    { name: 'Human', value: false},
+                    {name: 'Computer', value: true}
+                ]
+            });
+        } 
+
+        const game = new Game(computerOpponent);
+        game.playGame();
+    }
+
+    build() {
+        // Assign each cup on board typing and/or rules
+        // Assign store as one board location rather than two (or two with shared storage?)
+        // Will consist of a slice of each of the boards nested array
+        // player1: { smallCups: array[0][1...6], store: array[0][0]}
+        // player2: { smallCups: array[1][1...6], store: array[1][7]}
+        
+        // Add players -> create generic player and have all others inherit from it
+        // Assign players each a side and a store (side "closest" and store to the right)
+
+
+        // Distribute the stones into the 12 smallCups -> 4 stone per cup
+    }
+
+    playGame() {
 
     }
 }
