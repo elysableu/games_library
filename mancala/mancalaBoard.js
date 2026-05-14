9153833230c34ecf1ebe96bfc1cd4b3b03ca8d51
elysableu <elysabward@gmail.com>
2026-05-14 14:21:02 -0600

    Complete mancalaBoard class

--- mancala/mancalaBoard.js
+++ mancala/mancalaBoard.js
@@ -4,43 +4,47 @@ export class MancalaBoard extends PerimeterBoard {
     static DEFAULT_MANCALA_DIMENSIONS = {x: 7, y: 2};
 
     static DEFAULT_MANCALA_CONFIG = [
-        {name: 'store1', type: 'store'},
-        {name: 'cup1', type: 'cup'},
-        {name: 'cup2', type: 'cup'},
-        {name: 'cup3', type: 'cup'},
-        {name: 'cup4', type: 'cup'},
-        {name: 'cup5', type: 'cup'},
-        {name: 'cup6', type: 'cup'},
-        {name: 'store2', type: 'store'},
-        {name: 'cup7', type: 'cup'},
-        {name: 'cup8', type: 'cup'},
-        {name: 'cup9', type: 'cup'},
-        {name: 'cup10', type: 'cup'},
-        {name: 'cup11', type: 'cup'},
-        {name: 'cup12', type: 'cup'}
+        {name: 'cup1', type: 'cup', owner: 'Player1'},
+        {name: 'cup2', type: 'cup', owner: 'Player1'},
+        {name: 'cup3', type: 'cup', owner: 'Player1'},
+        {name: 'cup4', type: 'cup', owner: 'Player1'},
+        {name: 'cup5', type: 'cup', owner: 'Player1'},
+        {name: 'cup6', type: 'cup', owner: 'Player1'},
+        {name: 'store1', type: 'store', owner: 'Player1'},
+        {name: 'cup7', type: 'cup', owner: 'Player2'},
+        {name: 'cup8', type: 'cup', owner: 'Player2'},
+        {name: 'cup9', type: 'cup', owner: 'Player2'},
+        {name: 'cup10', type: 'cup', owner: 'Player2'},
+        {name: 'cup11', type: 'cup', owner: 'Player2'},
+        {name: 'cup12', type: 'cup', owner: 'Player2'},
+        {name: 'store2', type: 'store', owner: 'Player2'}
     ];
 
     constructor(dimensions = MancalaBoard.DEFAULT_MANCALA_DIMENSIONS, config = MancalaBoard.DEFAULT_MANCALA_CONFIG, cyclical = true) {
         super(dimensions, config, cyclical);
-        this.store1 = this.board[0];
-        this.store2 = this.board[7];
+        this.store1 = this.board[6];
+        this.store2 = this.board[13];
     }
 
     display() {
         const { x, y } = this.dimensions;
 
-        const cups1 = this.board.slice(1, x);
-        const cups2 = this.board.slice(x + 1, (2 * x) );
+        const cups1 = this.board.slice(0, x - 1);
+        const cups2 = this.board.slice(x, (2 * x) - 1);
 
         console.log(`           ${[...cups2].reverse().map(space => space.display()).join(' ')}`);
         console.log(`${this.store2.display()} ---------------------------------------------- ${this.store1.display()}`);
         console.log(`            ${cups1.map(space => space.display()).join(' ')}`);
     }
 
-    findOpposite(index) {
-
+    playerCups(player) {
+        return this.board.filter((space) => space.type === 'cup' && space.owner === player.name);
+    }
+    
+    findOpposite(space) {
+        const index = space.index;
+        const length = this.board.length;
+        const oppIndex = (length - index); 
+        return this.board[oppIndex];
     }
 }
-
-const manBoard = new MancalaBoard();
-manBoard.display();
